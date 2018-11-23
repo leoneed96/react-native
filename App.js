461c0f51674ca63bac5ea5af93486f0e52b87efb
Леонид Ершов <kindle47@outlook.com>
2018-11-23 19:17:07 +0300

    +

--- App.js
+++ App.js
@@ -7,25 +7,20 @@ import {
   View,
   Button
 } from "react-native";
-import {createStackNavigator, createAppContainer} from 'react-navigation';
-import Home from "./Home"
-import Window from "./Window"
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Home from "./Home";
+import Window from "./Window";
 
 const Nav = createStackNavigator({
-  Home: {screen: Home},
-  Window: {screen: Window}
+  Home: { screen: Home },
+  Window: { screen: Window }
   // Profile: {screen: ProfileScreen},
 });
 const App = createAppContainer(Nav);
-
-
 export default App;
-
-
 
 class Reusable extends React.Component {
   render() {
     return <Text>Hello World! My prop is {this.props.param}.</Text>;
   }
 }
-
